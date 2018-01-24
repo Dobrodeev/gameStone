42bbef531d1fc2541ede276d6f7ddd40b2b50272
Dobrodeev <dobrod@meta.ua>
2018-01-25 01:08:41 +0200

    we add function IsEndGame() and now we will pley to 7 wins

--- Game 3.php
+++ Game 3.php
@@ -4,18 +4,39 @@
  * @author Valera
  * @copyright 2017
  */
- function ConsoleReadln()
-{        
+function RandUnitFrom($units)
+{
+    //$units=array("Player1","Player2","Player3","Player4","Player5");
+    //$unit_wins=array(0,0,0,0,0);
+    $index_rand=rand(0,count($units)-1);
+    $rand_player=$units[$index_rand];
+    return $rand_player;
+}
+function GetNumber($player, $players)
+{
+    for($i=0; $i<count($players); $i++)
+    {
+        if($players[$i]==$player)
+            return $i;
+    }
+}
+function SaveWin($res,&$unit_wins, $player1, $player2, $players)
+{
+    if($res=="Unit1 win") $unit_wins[GetNumber($player1, $players)]++;
+    elseif($res=="Unit2 win") $unit_wins[GetNumber($player2, $players)]++;
+}
+function ConsoleReadln()
+{
         switch($x)
         {
-            case 0: 
-            $variant=0;// Ножницы
+            case 0:
+            $variant=0;// РќРѕР¶РЅРёС†С‹
             break;
             case 1:
-            $variant=1;// Бумага
+            $variant=1;// Р‘СѓРјР°РіР°
             break;
             case 2:
-            $variant=2;// Колодец
+            $variant=2;// РљРѕР»РѕРґРµС†
             break;
         }
 }
@@ -23,18 +44,18 @@ function ConsoleWriteln($unit1)
 {
         switch($unit1)
         {
-            case 0: 
-            print("Ножницы <br>");// Ножницы
+            case 0:
+            print("РќРѕР¶РЅРёС†С‹ <br>");// РќРѕР¶РЅРёС†С‹
             break;
             case 1:
-            print("Бумага <br>");// Бумага
+            print("Р‘СѓРјР°РіР° <br>");// Р‘СѓРјР°РіР°
             break;
             case 2:
-            print("Колодец <br>");// Колодец
+            print("РљРѕР»РѕРґРµС† <br>");// РљРѕР»РѕРґРµС†
             break;
         }
 }
-function RandUsers()
+function RandRes()
 {
     $unit1=rand(0,2);
     $unit2=rand(0,2);
@@ -61,11 +82,61 @@ function CompareUnits($Unit1,$Unit2)
     elseif($Unit1==2 && $Unit2==1) return "Unit2 win";
     return "no any fjbjfbjhbjhjhb here";
 }
+function IsEndGame($wins, $max_win)
+{
+    foreach($wins as $n)
+        if($n>=$max_win)
+            return true;
+
+    return false;
+}
+function ShowStats($wins, $player1, $player2, $players)
+{
+    for($i=0; $i<count($players); $i++)
+    {
+        print($i." ".$wins[$i]." ");
+        $i1 =GetNumber($player1, $players);
+        $i2 =GetNumber($player2, $players);
+        print( (($i==$i1) || ($i==$i2)) ? " *** " : "     ");
+        print("<br>");
+    }
+}
 function Main()
 {
-    $u1=RandUsers();
-    $res = CompareUnits($u1[0],$u1[1]);
-    print($res."<br>");
+    $NumberOfPlayers = rand(4, 10);
+
+    //init
+    $players = array();
+    $wins = array();
+    for($i=0; $i<$NumberOfPlayers; $i++)
+    {
+        $players[$i] = "Player".($i+1);
+        $wins[$i] = 0;
+    }
+    $NeedsWins = 7;
+
+    //game
+    $Rounds = 0;
+    do
+    {
+        $Rounds++;
+        print("<br><br>Round".$Rounds."<br>");
+        $player1 = RandUnitFrom($players);
+        $player2;
+        do
+        {
+           $player2 = RandUnitFrom($players);
+        }
+        while($player2==$player1);
+        print($player1."  vs ".$player2."<br>");
+        $u1=RandRes();
+        $res = CompareUnits($u1[0],$u1[1]);
+        SaveWin($res, $wins, $player1, $player2, $players);
+        print($res."<br>");
+        ShowStats($wins, $player1, $player2, $players);
+    }
+    while(!IsEndGame($wins, $NeedsWins));
 }
 Main();
+
 ?>
