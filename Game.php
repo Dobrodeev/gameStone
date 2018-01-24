4b39251fa44aa4ae5e7d4387e5e30f3f22a040aa
Dobrodeev <dobrod@meta.ua>
2018-01-25 00:55:44 +0200

    we add function IsCanMove() and now man cannot go out from field

--- Game.php
+++ Game.php
@@ -5,8 +5,7 @@
  * @copyright 2017
  */
 
-
-function Array2($height, $width)
+function GenerateMap($height, $width)
 {
     $arr=array();
     for($i=0;$i<$height;$i++)
@@ -14,7 +13,12 @@ function Array2($height, $width)
         $arr[$i]=array();
         for($j=0;$j<$width;$j++)
         {
-            $arr[$i][$j]=0;
+            if(rand(0,5)==0)
+            {
+                $arr[$i][$j]="0";
+                continue;
+            }
+            $arr[$i][$j]="*";
         }
     }
     return $arr;
@@ -24,7 +28,7 @@ function Screen($arr, $x, $y)
     for($i=0; $i<count($arr); $i++)
     {
         for($j=0;$j<count($arr[$i]);$j++)
-        {   
+        {
             if($i==$y && $j==$x)
             {
                 print("#");
@@ -45,46 +49,78 @@ function GetUserInput()
         case 1: return "DOWN";
         case 2: return "LEFT";
         case 3: return "RIGHT";
+        default: return "";
     }
-    return "";
 }
 function PlayerInit($w, $h, &$x, &$y)
 {
     $x = rand(0, $w-1);
     $y = rand(0, $h-1);
 }
+function IsWalkable($str)
+{
+    switch($str)
+    {
+        case "0":
+        return false;
+
+        default: return true;
+    }
+}
+function IsCanMove($command, &$x, &$y, $map)
+{
+    //bounds
+    if($y==0 && $command=="UP") return false;
+    if($y==9 && $command=="DOWN")return false;
+    if($x==0 && $command=="LEFT") return false;
+    if($x==9 && $command=="RIGHT") return false;
+
+    //walkable
+    $X=$x;
+    $Y=$y;
+    Move($command, $X, $Y);
+    if(!IsWalkable($map[$Y][$X])) return false;
+
+    //all ok
+    return true;
+}
 function Move($command, &$x, &$y)
 {
-    switch($command)
-    {
-        case "UP":
+        switch($command)
+        {
+            case "UP":
             $y--;
             break;
-        case "DOWN":
+            case "DOWN":
             $y++;
             break;
-        case "LEFT":
+            case "LEFT":
             $x--;
             break;
-        case "RIGHT":
+            case "RIGHT":
             $x++;
             break;
-    }
+        }
+
 }
 function Main()
 {
     $W = 10;
     $H = 10;
-    $map = Array2($W,$H);
+    $map = GenerateMap($W,$H);
     PlayerInit($W, $H, $X, $Y);
     $totalTime = 45;
     Screen($map, $X, $Y);
     for($t =0; $t<$totalTime; $t++)
     {
         $userInput = GetUserInput();
-        Move($userInput, $X, $Y);
+        if(IsCanMove($userInput, $X, $Y, $map))
+        {
+            Move($userInput, $X, $Y);
+        }
         Screen($map, $X, $Y);
     }
 }
 Main();
+
 ?>
