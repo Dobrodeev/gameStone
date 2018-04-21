203f2aba2be81244c0bd2d41258b5a947ad80712
Dobrodeev <dobrod@meta.ua>
2018-04-21 20:21:57 +0300

    избавились от магических чисел x, y, x2, y2, заменили их на осмысленные

--- scriptGame.php
+++ scriptGame.php
@@ -13,51 +13,26 @@
 	<div class="bordered">
 		<h3>Результат</h3>
 		<?php
-		$y = $_POST['day_s'];
-			if($y == 1) $y2 = 'Ножницы';
-			elseif($y == 2) $y2 = 'Бумага';
-			elseif($y == 3) $y2 = 'Колодец';
-		echo '<p>Вы выбрали вариант: <b>'.$y2.'</b></p>';
-		$x = rand(1,3);
-			if($x ==1 ) $x2 = 'Ножницы';
-			elseif($x == 2) $x2 = 'Бумага';
-			elseif($x == 3) $x2 = 'Колодец';
-		echo '<p>Компьютер(Bot) выбрал вариант: <b>'.$x2.'</b></p>';
-		if($y == $x)
+		$varUser = $_POST['variant'];
+			if($varUser == 1) $stringUser = 'Ножницы';
+			elseif($varUser == 2) $stringUser = 'Бумага';
+			elseif($varUser == 3) $stringUser = 'Колодец';
+		echo '<p>Вы выбрали вариант: <b>'.$stringUser.'</b></p>';
+		$varBot = rand(1,3);
+			if($varBot == 1 ) $stringBot = 'Ножницы';
+			elseif($varBot == 2) $stringBot = 'Бумага';
+			elseif($varBot == 3) $stringBot = 'Колодец';
+		echo '<p>Компьютер(Bot) выбрал вариант: <b>'.$stringBot.'</b></p>';
+		if($varUser == $varBot)
 			echo '<p>Draw</p>';
-		elseif($y == 1 && $x == 2)
+		elseif($varUser == 1 && $varBot == 2)
 			echo '<p>You win</p>';
-		elseif($y == 1 && $x == 3)
+		elseif($varUser == 1 && $varBot == 3)
 			echo '<p>Bot win</p>';
-		elseif($y == 2 && $x == 3)
+		elseif($varUser == 2 && $varBot == 3)
 			echo '<p>You win</p>';
 		?>
-	<a href="Game Form for ЮЗФ.htm">Next round</a>
-	</div>
-	<hr>
-<div class="row">
-  <div class="span4">Страница 1</div>
-  <div class="span8">Страница 2</div>
-</div>
-<hr>
-<div class="container-fluid">
-  <div class="row-fluid">
-    <div class="span2">
-      <!--Sidebar content-->
-      <p>Text 1</p>
-      <p>Text 2</p>
-      <p>Text 3</p>
-      <p>Text 4</p>
-    </div>
-    <div class="span10">
-      <!--Body content-->
-      <p>Text 1</p>
-      <p>Text 2</p>
-      <p>Text 3</p>
-      <p>Text 4</p>
-    </div>
-  </div>
-</div>
+	<a href="GameForm .htm">Next round</a>
 </body>
 </html>
 
