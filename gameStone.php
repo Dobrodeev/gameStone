0accc72f3f03e480c23b3ae8ed5e7939d1892597
Dobrodeev <dobrod@meta.ua>
2018-04-21 20:54:00 +0300

    создали отдельно форму для игры

---
+++ gameStone.php
@@ -0,0 +1,33 @@
+<html>
+<head>
+    <meta charset="utf-8">
+    <link rel="stylesheet" href="assetst/reset.css">
+    <link href="assets/css/bootstrap.min.css" rel="stylesheet" media="screen">
+    <title>Игра ЮЗФ</title>
+</head>
+<body>
+<script src="http://code.jquery.com/jquery-latest.js"></script>
+<script src="assets/js/bootstrap.min.js"></script>
+<div>
+    <form name="form1" method="post" action="scriptGame.php">
+        <h3>Game JuZeFa for webdesign</h3>
+        <hr size="1">
+        <p>Выбираем вариант:</p>
+        <p>
+            <br>
+            <select name=variant size=1>
+                <option value=1>Ножницы</option>
+                <option value=2>Бумага</option>
+                <option value=3 selected>Колодец</option>
+            </select>
+        </p>
+        <br>
+        <p>
+            <input type="submit" value="Отослать форму">
+            &nbsp;&nbsp;&nbsp<input type="reset" value="Очистить форму">
+        </p>
+    </form>
+</div>
+</body>
+<!--<script>document.write('<script src="http://' + (location.host || 'localhost').split(':')[0] + ':35729/livereload.js?snipver=1"></' + 'script>')</script>-->
+</html>
