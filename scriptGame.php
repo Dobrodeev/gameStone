603f4f3d494c9652a66515690984de0a60b2668f
Dobrodeev <dobrod@meta.ua>
2018-04-21 21:53:14 +0300

    реализовали подсчет очков: drawe 0.5 - 0.5; win 1

--- scriptGame.php
+++ scriptGame.php
@@ -14,7 +14,8 @@
 		<h3>Результат</h3>
 		<?php
 //        include 'gameStone.php';
-		$varUser = $_POST['variant'];
+		define('WIN_LIMIT', 10);
+        $varUser = $_POST['variant'];
         $varBot = rand(1,3);
 
 		function writeVariant($var)
@@ -38,17 +39,32 @@
         {
             $varUser = $user;
             $varBot = $bot;
+            global $counterUser, $counterBot;
+            $counterUser = $counterBot = 0;
             if($varUser == $varBot)
+            {
                 echo '<p>Draw</p>';
+                $counterUser += 0.5;
+                $counterBot += 0.5;
+            }
+
             elseif(($varUser == 1 && $varBot == 2) || ($varUser == 2 && $varBot == 3) ||
                 ($varUser == 3 && $varBot == 1))
+            {
                 echo '<p>You win</p>';
+                $counterUser++;
+            }
+
             else
+            {
                 echo '<p>Bot win</p>';
+                $counterBot++;
+            }
         }
 
         writeChange();
-        compareVariant($varUser, $varBot);
+		compareVariant($varUser, $varBot);
+		echo 'You wins: '.$counterUser.' Bot wins: '.$counterBot.'<br>';
 		?>
 	<a href="GameForm .htm">Next round</a>
 </body>
