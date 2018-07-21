b0fdbb94933c381bb8b530ba1b6454060fd86276
dobrodeev <dobrod@meta.ua>
2018-07-21 20:28:29 +0300

    вывели таблицу использую классы Bootstrap

---
+++ probability_theory.php
@@ -0,0 +1,27 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+    <link href="assets/css/bootstrap.min.css" rel="stylesheet" media="screen">
+</head>
+<body>
+<script src="assets/jquery-3.2.1.js"></script>
+<script src="assets/js/bootstrap.min.js"></script>
+<h5>Результаты нескольких опытов бросания монеты</h5>
+<!--<table class="table table-striped table-bordered table-hover">-->
+<table class="table table-condensed">
+    <tr><th>Число бросаний</th><th>Число появлений герба</th><th>Относительная частота</th></tr>
+    <tr><td>4040</td><td>2048</td><td>0,5069</td></tr>
+    <tr><td>12000</td><td>6019</td><td>0,5016</td></tr>
+    <tr><td>24000</td><td>12012</td><td>0,5005</td></tr>
+
+</table>
+<?php
+?>
+</body>
+</html>
+
