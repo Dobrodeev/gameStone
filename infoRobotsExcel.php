1bb2aeff2fc83eba9dc95f168e44a1b187769a30
dobrodeev <dobrod@meta.ua>
2018-08-15 14:11:28 +0300

    add different arrays of styles

--- infoRobotsExcel.php
+++ infoRobotsExcel.php
@@ -43,71 +43,83 @@ session_start();
                      'style'=> PHPExcel_Style_Border::BORDER_THIN,
                      'color'=>array('rgb'=>'909090')
                  )
-             ),
-             'alignment'=>array(
-                 'horizontal'=> PHPExcel_Style_Alignment::HORIZONTAL_CENTER,
-                 'vertical'=>PHPExcel_Style_Alignment::VERTICAL_CENTER
-             ),
-             'font'=>array(
-                 'name'=>'Calibri',
-                 'size'=>11,
-                 'italic'=>true
              )
          );
+        $style_center = array(
+            'alignment'=>array(
+                'horizontal'=> PHPExcel_Style_Alignment::HORIZONTAL_CENTER,
+                'vertical'=>PHPExcel_Style_Alignment::VERTICAL_CENTER
+            )
+        );
+        $style_left = array(
+            'alignment'=>array(
+                'horizontal'=> PHPExcel_Style_Alignment::HORIZONTAL_LEFT,
+                'vertical'=>PHPExcel_Style_Alignment::VERTICAL_CENTER
+            )
+        );
          // Создать новый массив стилей, чтобы он был таким же как в примере тестового задания
-         /**
-         'font'=>array(
-    'bold'=>true,
-    'italic'=>true,
-          */
         $style_ok = array(
             'fill'=>array(
                 'type'=> PHPExcel_Style_Fill::FILL_SOLID,
-                'color'=>array('rgb'=>'33FF33')
+                'color'=>array('rgb'=>'93c47d')
             )
         );
         $style_error = array(
             'fill'=>array(
                 'type'=> PHPExcel_Style_Fill::FILL_SOLID,
-                'color'=>array('rgb'=>'f48642')
+                'color'=>array('rgb'=>'e06666')
+            )
+        );
+        $style_header = array(
+            'fill'=>array(
+                'type'=> PHPExcel_Style_Fill::FILL_SOLID,
+                'color'=>array('rgb'=>'a2c4c9')
+            ),
+            'font'=>array(
+                'name'=>'Arial',
+                'size'=>10,
+                'bold'=>true,
+                'italic'=>false
             )
         );
         $style_between = array(
             'fill'=>array(
-                'type'=> PHPExcel_Style_Fill::FILL_PATTERN_DARKDOWN,
-                'color'=>array('rgb'=>'8b008b')
+                'type'=> PHPExcel_Style_Fill::FILL_SOLID,
+                'color'=>array('rgb'=>'efefef'),
             )
         );
 
         $objExcel = new PHPExcel();
         $objExcel->setActiveSheetIndex(0);
-        //$objExcel->createSheet();
 
         $active_sheet = $objExcel->getActiveSheet();
         $active_sheet->setTitle("Test Table");
         $active_sheet->getPageSetup()->setPaperSize(PHPExcel_Worksheet_PageSetup::PAPERSIZE_A4);
         $active_sheet->getPageSetup()->setOrientation(PHPExcel_Worksheet_PageSetup::ORIENTATION_PORTRAIT);
 
-        $active_sheet->mergeCells('A1:C1');
+        $active_sheet->getColumnDimension('B')->setWidth(60);
+        $active_sheet->getColumnDimension('B')->setWidth(30);
 
-        $active_sheet->getColumnDimension('B')->setWidth(80);
-        $active_sheet->getRowDimension('1')->setRowHeight(30);
+        $active_sheet->setCellValue('A1', '№');
+        $active_sheet->setCellValue('B1', 'Название проверки');
+        $active_sheet->setCellValue('C1', 'Статус');
 
-        $active_sheet->getStyle('C')->getNumberFormat()->setFormatCode(PHPExcel_Style_NumberFormat::FORMAT_TEXT);
 
+        $objExcel->getActiveSheet()->getStyle('A:C')->applyFromArray($style_center);
+        $objExcel->getActiveSheet()->getStyle('B')->applyFromArray($style_left);
+        $objExcel->getActiveSheet()->getStyle('A1:C1')->applyFromArray($style_header);
+        $active_sheet->getStyle('1')->getNumberFormat()->setFormatCode(PHPExcel_Style_NumberFormat::FORMAT_GENERAL);
 
-        $active_sheet->setCellValue('A1', 'Результат тестирования robots.txt:');
-        $active_sheet->setCellValue('A2', 'id');
-        $active_sheet->setCellValue('B2', 'check');
-        $active_sheet->setCellValue('C2', 'status');
-//        $active_sheet->setCellValue('D2', 'price');
-        $num_row = 3;
+        $num_row = 2;
         foreach ($data as $value)
         {
+            $active_sheet->mergeCells('A'.$num_row.':C'.$num_row);
+            $active_sheet->getStyle('A'.$num_row.':C'.$num_row)->applyFromArray($style_between);
+            $num_row ++;
+            $active_sheet->getRowDimension($num_row)->setRowHeight(40);
             $active_sheet->setCellValueByColumnAndRow(0, $num_row , $value['id']);
             $active_sheet->setCellValueByColumnAndRow(1, $num_row , $value['check']);
             $active_sheet->setCellValueByColumnAndRow(2, $num_row , $value['status']);
-//            $objExcel->getActiveSheet()->getStyle('A-C', ++$num_row)->applyFromArray($style_between);
             if ($value['status'] == 'OK')
             {
                 $objExcel->getActiveSheet()->getStyle('C'.$num_row)->applyFromArray($style_ok);
@@ -117,14 +129,11 @@ session_start();
                 $objExcel->getActiveSheet()->getStyle('C'.$num_row)->applyFromArray($style_error);
             }
             // после каждой строки результата сделать пустую строку серого цвета
-//            $active_sheet->setCellValueByColumnAndRow(3, $num_row , $value['price']);
-            //$active_sheet->getStyle('D'.$num_row)->applyFromArray($style_ok);
-            $num_row++;
+            $num_row ++;
         }
 //        $active_sheet->getStyle();
         $active_sheet->getStyle('A1:C'.($num_row-1))->applyFromArray($style_arr);
-        //$objExcel->getActiveSheet()->getStyle('C3:C8')->applyFromArray($style_ok);
-        //$objExcel->getActiveSheet()->getStyle('C7:C8')->applyFromArray($style_error);
+
 
         header("Content-Type: application/vnd.ms-excel");
         header("Content-Disposition: attachment; filename='our_table.xlsx'");
