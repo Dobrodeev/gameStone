fa8ea4681fe763446847468cebfc14308f56915d
Dobrodeev <dobrod@meta.ua>
2018-09-06 02:06:23 +0300

    созали файл с формой оьдельно оь обработчика

---
+++ factorialForm.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Game result</title>
+	<!-- <link rel="stylesheet" href="assets/style.css"> -->
+	<link rel="stylesheet" href="assets/reset.css">
+	<link href="assets/css/bootstrap.min.css" rel="stylesheet" media="screen">
+</head>
+<body>
+	<script src="assets/jquery-3.2.1.js"></script>
+	<script src="assets/js/bootstrap.min.js"></script>
+<form action="factorial_handler.php" method="post">
+  <!--
+  <div class="form-group">  
+    <label for="exampleInputEmail1">Email address</label>
+    <input type="text" class="form-control" id="exampleInputEmail1" placeholder="Email">
+  </div>
+  <div class="form-group">
+    <label for="exampleInputPassword1">Password</label>
+    <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password">
+  </div>
+  <div class="form-group">
+    <label for="exampleInputFile">File input</label>
+    <input type="file" id="exampleInputFile">
+    <p class="help-block">Example block-level help text here.</p>
+  </div>
+  <div class="checkbox">
+    <label>
+      <input type="checkbox"> Check me out
+    </label>
+  </div>  
+    -->
+    <div class="form-group">  
+        <label for="exampleInputEmail1">Number Factorial!</label>
+        <input type="text" class="form-control" id="exampleInputEmail1" placeholder="number" mame="number">
+    </div>  
+  <button type="submit" class="btn btn-default" name="go">Submit</button>
+</form>
+</body>
+</html>
