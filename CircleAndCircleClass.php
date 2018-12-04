e5aaea8dea58743411fbcbdb5fe65fb87a6a2116
dobrodeev <dobrod@meta.ua>
2018-12-04 10:09:38 +0200

    убрали лишнее свойство public $counterIntersection. Метод getCircle() переименовали на setCircle(). Добавили метод public function countIntersections($another), который считает количесво пересечений двух окружностей. В этом методе $x1 изменили на $x, а $x2 - на $another->x. Добавили магический метод __construct($x, $y, $r), который принимает координаты центра и радиус окружности, setCircle() теперь не нужен.

--- CircleAndCircleClass.php
+++ CircleAndCircleClass.php
@@ -8,19 +8,24 @@
 
 class CircleAndCircleClass
 {
-
     private $x;
     private $y;
     private $r;
-    public $countPoints;
-    
-    public function getCircle($x, $y, $r)
+
+    public function __construct($x, $y, $r)
     {
         $this->x = $x;
         $this->y = $y;
         $this->r = $r;
     }
-    
+
+    public function setCircle($x, $y, $r)
+    {
+        $this->x = $x;
+        $this->y = $y;
+        $this->r = $r;
+    }
+    /**
     public function countIntersections()
     {
         $x1 = $this->x;
@@ -28,45 +33,62 @@ class CircleAndCircleClass
         $r1 = $this->r;
         echo 'Circle: ('.$x1.', '.$y1.'); r='.$r1.'<br>';
     }
-    
-    public function countIntersectionsCompare()
+    */
+    public function countIntersections($another)
     {
-        
+        // abs(), min(), max() if $distance > abs(r1-r2)
+        $distance = sqrt(pow($this->x - $another->x, 2) + pow($this->y1 - $another->y, 2));
+        if ($distance > $this->r + $another->r || $another->r > $distance + $this->r || $this->r > $distance + $another->r)
+        {
+            return 0;
+        }
+        elseif ($distance == $this->r + $another->r || $another->r == $distance + $this->r || $this->r == $distance + $another->r)
+        {
+            return 1;
+        }
+        elseif ($distance < $this->r + $another->r || $another->r < $distance + $this->r || $this->r < $distance + $another->r)
+        {
+            return 2;
+        }
+        elseif ($this->x == $another->x && $this->y == $another->y && $this->r == $another->r)
+        {
+            return -1;
+        }
     }
     // 1 класс для круга и 2 обьекта - круги
     /**
     public function getCircle1($x, $y, $r)
     {
 
-        $this->x1 = $x;
-        $this->y1 = $y;
-        $this->r1 = $r;
+        $this->x = $x;
+        $this->y = $y;
+        $this->r = $r;
     }
 
     public function getCircle2($x, $y, $r)
     {
-        $this->x2 = $x;
-        $this->y2 = $y;
-        $this->r2 = $r;
+        $another->x = $x;
+        $another->y = $y;
+        $another->r = $r;
     }
 
     public function countIntersections()
     {
         // abs(), min(), max() if $distance > abs(r1-r2)
-        $distance = sqrt(pow($this->x1 - $this->x2, 2) + pow($this->y1 - $this->y2, 2));
-        if ($distance > $this->r1 + $this->r2 || $this->r2 > $distance + $this->r1 || $this->r1 > $distance + $this->r2)
+        $distance = sqrt(pow($this->x - $another->x, 2) + pow($this->y - $another->y, 2));
+        if ($distance > $this->r + $another->r || $another->r > $distance + $this->r || $this->r > $distance + $another->r)
         {
             $this->countPoints = 0;
         }
-        elseif ($distance == $this->r1 + $this->r2 || $this->r2 == $distance + $this->r1 || $this->r1 == $distance + $this->r2)
+        elseif ($distance == $this->r + $another->r || $another->r == $distance + $this->r || $this->r == $distance + $another->r)
         {
             $this->countPoints = 1;
         }
-        elseif ($distance < $this->r1 + $this->r2 || $this->r2 < $distance + $this->r1 || $this->r1 < $distance + $this->r2)
+        elseif ($distance < $this->r + $another->r || $another->r < $distance + $this->r || $this->r < $distance + $another->r)
         {
             $this->countPoints = 2;
         }
-        elseif ($this->x1 == $this->x2 && $this->y1 == $this->y2 && $this->r1 == $this->r2)
+        elseif ($this->x == $another->x && $this->y == $another->y && $this->r == $another->r)
         {
             $this->countPoints = 'infinity';
         }
