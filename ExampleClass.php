de3d162913b3f2514a7dde2d97eaa46c4cce059a
dobrodeev <dobrod@meta.ua>
2018-12-06 14:21:50 +0200

    сделали до конца все задачи на массивы, определили некорректное условие в заданиях 10 & 11 - произведение номеров элементов получается ноль из-за умножения на нулевой индекс

--- ExampleClass.php
+++ ExampleClass.php
@@ -238,7 +238,7 @@ class ExampleClass
      */
     public function exercisesArrays()
     {
-        $simpleArray = [49, -13, 26, -3, 19, 29, 32, 27, -20, 18];
+        $simpleArray = [49, -13, 26, -3, 19, 29, 3, 27, -20, 18];
         echo '<pre>';
         print_r($simpleArray);
         echo '</pre>';
@@ -252,5 +252,125 @@ class ExampleClass
             }
         }
         echo 'Сумма положительных элементов массива: '.$Summa.'<br>';
+        $Summa = 0;
+        for ($i = 0; $i < count($simpleArray); $i++)
+        {
+            if ($simpleArray[$i] < 0)
+            {
+                $Summa += $simpleArray[$i];
+            }
+        }
+        echo 'Сумма отрицательных элементов массива: '.$Summa.'<br>';
+        $Proizvedenie = 1;
+        for ($i = 0; $i < sizeof($simpleArray); $i++)
+        {
+            if ($simpleArray[$i] > 0)
+            {
+                $Proizvedenie *= $simpleArray[$i];
+            }
+        }
+        echo 'Произведение положительных элементов массива: '.$Proizvedenie.'<br>';
+        $Proizvedenie = 1;
+        for ($i = 0; $i < sizeof($simpleArray); $i++)
+        {
+            if ($simpleArray[$i] < 0)
+            {
+                $Proizvedenie *= $simpleArray[$i];
+            }
+        }
+        echo 'Произведение отрицательных элементов массива: '.$Proizvedenie.'<br>';
+        echo 'элементы массива, которые > 5:<br>';
+        for ($i = 0; $i < sizeof($simpleArray); $i++)
+        {
+            if ($simpleArray[$i] > 5)
+            {
+                echo $simpleArray[$i].' ';
+            }
+        }
+        echo '<br>';
+        echo 'Вторая половина массива:<br>';
+        for ($i = count($simpleArray)/2; $i < sizeof($simpleArray); $i++)
+        {
+                echo $simpleArray[$i].' ';
+        }
+        echo '<br>Количество положительных элементов массива:<br>';
+        $num = 0;
+        for ($i = 0; $i < count($simpleArray); $i++)
+        {
+            if ($simpleArray[$i] > 0)
+            {
+                $num++;
+            }
+        }
+        echo 'Всего '.$num.' положительных элементов<br>';
+        $num = 0;
+        for ($i = 0; $i < count($simpleArray); $i++)
+        {
+            if ($simpleArray[$i] < 0)
+            {
+                $num++;
+            }
+        }
+        echo 'Всего '.$num.' отрицательных элементов<br>';
+        $Proizvedenie = 1;
+        for ($i = 0; $i < sizeof($simpleArray); $i++)
+        {
+            if ($simpleArray[$i] > 0)
+            {
+                $Proizvedenie *= $i;
+            }
+        }
+        echo 'Произведение положительных номеров элементов массива: '.$Proizvedenie.'<br>';
+        $Proizvedenie = 1;
+        for ($i = 0; $i < sizeof($simpleArray); $i++)
+        {
+            if ($simpleArray[$i] < 0)
+            {
+                $Proizvedenie *= $i;
+            }
+        }
+        echo 'Произведение отрицательных номеров элементов массива: '.$Proizvedenie.'<br>';
+        $minElement = min($simpleArray);
+        for ($i = 0; $i < count($simpleArray); $i++)
+        {
+            if ($simpleArray[$i] == $minElement)
+            {
+//                echo 'Номер min элемента: '.key($simpleArray).'<br>';
+                echo 'Номер min элемента: '.$i.'<br>';
+            }
+        }
+        $maxElement = max($simpleArray);
+        for ($i = 0; $i < count($simpleArray); $i++)
+        {
+            if ($simpleArray[$i] == $maxElement)
+            {
+                echo 'Номер max элемента: '.$i.'<br>';
+            }
+        }
+        /**
+         * Средрее арифметическое элементов массива
+         */
+        $avgSumma = 0;
+        for ($i = 0; $i < count($simpleArray); $i++)
+        {
+            $avgSumma += abs($simpleArray[$i]);
+        }
+        echo 'Среднее арифметическое: '.$avgSumma/count($simpleArray).'<br>';
+        echo 'Элементы которые < 10<br>';
+        for ($i = 0; $i < count($simpleArray); $i++)
+        {
+            if ($simpleArray[$i] < 10)
+            {
+                echo $simpleArray[$i].' ';
+            }
+        }
+        echo '<br>Элементы которые 0< x <6<br>';
+        for ($i = 0; $i < count($simpleArray); $i++)
+        {
+            if ($simpleArray[$i] > 0 && $simpleArray[$i] < 6)
+            {
+                echo $simpleArray[$i].' ';
+            }
+        }
     }
 }
