90c5436a479d22ed4558bc311072e7b12e3ba643
Dobrodeev <dobrod@meta.ua>
2019-02-11 23:14:22 +0200

    добавили файл соединение с БД statistics

---
+++ DB.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Zver
+ * Date: 11.02.2019
+ * Time: 22:35
+ */
+
+$host = '127.0.0.1';
+$db   = 'statistics';
+$user = 'root';
+$pass = '';
+$charset = 'utf8';
+
+$dsn = "mysql:host=$host;dbname=$db;charset=$charset";
+$opt = [
+    PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION,
+    PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+    PDO::ATTR_EMULATE_PREPARES   => false,];
+$pdo = new PDO($dsn, $user, $pass, $opt);
