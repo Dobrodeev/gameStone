df53836cafa0c401b2020e745ee45b4cf6dcdf32
Dobrodeev <dobrod@meta.ua>
2019-04-14 22:12:53 +0300

    реализовали вьезд и выезд на стоянку: при вьезде значение столбца on_parking=y, при выезде on_parking=n

--- carParking.php
+++ carParking.php
@@ -13,7 +13,7 @@
 <script src="assets/js/bootstrap.min.js"></script>
 <form action="#" method="post">
     <input class="form-control form-control-lg" type="text" placeholder="7:00" name="time"><br>
-    <input class="form-control form-control-lg" type="text" placeholder="AA0000AM" name="car"><br>
+<!--    <input class="form-control form-control-lg" type="text" placeholder="AA0000AM" name="car"><br>-->
     <button type="submit" class="btn btn-primary" name="go">Submit</button>
 </form>
 <?php
@@ -23,27 +23,44 @@
  * Date: 26.03.2019
  * Time: 17:56
  */
+/*7:00 * CAR1
+-Прибытие
+7:10 * CAR2
+-Прибытие
+7:20 * CAR3
+-Прибытие
+7:20 * CAR1
+-Отбытие, общая парковка: 0:20
+7:33 * CAR2
+-Отбытие, всего парковок: 0:23*/
 include 'DB.php';
 if (isset($_POST['go']))
 {
     $time = $_POST['time'];
-    $car = $_POST['car'];
     $hours =  1*$time;
-    $minutes = substr($time, strpos($time,':') + 1);
+    $minutes = substr($time, strpos($time,':') + 1,2);
+    $car = substr($time, strpos($time, '*') + 1);
     echo 'hiurs: '.$hours.'<br>';
     echo 'min: '.$minutes.'<br>';
     echo 'car: '.$car.'<br>';
-    /*$queryCheck = "SELECT * FROM parking WHERE car_name='$car'";
+    echo 'car: '.$car.'<br>';
+    $queryCheck = "SELECT * FROM parking WHERE car_name='$car'";
     $check = $pdo->query($queryCheck)->fetch();
-    print_r($check);
+//    print_r($check);
     if (!$check)
     {
-        $queryCome = "INSERT INTO parking (car_name, car_time) VALUES ('$car', '$time')";
-        $queryOut = "";
+        $on_parking = 'y';
+        $queryCome = "INSERT INTO parking (car_time, on_parking, car_name) VALUES ('$time','$on_parking','$car')";
+
         $result = $pdo->query($queryCome);
-        echo 'Машина '.$car.' прибыла на парковку в '.$time.'<br>';
     }
     else
-        echo 'Машина уехала со стоянки в:';*/
+    {
+        $on_parking = 'n';
+        $queryOut = "UPDATE parking SET on_parking='$on_parking' WHERE car_name='$car'";
+        $result = $pdo->query($queryOut);
+        echo 'Машина уехала со стоянки в:';
+    }
+
 }
 ?>
