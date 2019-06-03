55b07002191024e96c839fce0add6c870d2fbe0e
Dobrodeev <dobrod@meta.ua>
2019-06-03 12:40:54 +0300

    создали каласс Car в котором описан автомобиль, который будет ставиться на стоянку

---
+++ CarOnParking.php
@@ -0,0 +1,30 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Zver
+ * Date: 13.05.2019
+ * Time: 11:40
+ */
+
+class CarOnParking
+{
+    public $car_name;
+    public $car_on_parking;
+    public $car_arrive;
+    public $car_depart;
+    public $car_time;
+
+    /**
+     * CarOnParking constructor.
+     * @param $car_name
+     * @param $car_on_parking
+     */
+    public function __construct($car_name, $car_on_parking)
+    {
+        $this->car_name = $car_name;
+        $this->car_on_parking = $car_on_parking;
+    }
+
+    public function 
+
+}
