c59cf6a01ec1316af8c3af6bd145648e9d33dcbb
Dobrodeev <dobrod@meta.ua>
2021-01-05 22:50:21 +0200

    скрипт для анализа robots.txt на ошибки

---
+++ infoRobots/infoRobots.php
@@ -0,0 +1,133 @@
+<?php
+session_start();
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Game result</title>
+    <link rel="stylesheet" href="../assets/style.css">
+    <!--    <link rel="stylesheet" href="assets/reset.css">-->
+    <link href="../assets/css/bootstrap.min.css" rel="stylesheet" media="screen">
+</head>
+<body>
+<script src="../assets/jquery-3.2.1.js"></script>
+<script src="../assets/js/bootstrap.min.js"></script>
+<h3>Анализ robots.txt</h3>
+<form action="#" method="get">
+    <div class="form-group">
+        <label for="exampleInputUrl">url address</label>
+        <input type="url" class="form-control" id="exampleInputEmail1" placeholder="host" name="host">
+    </div>
+    <button type="submit" class="btn btn-default">Анализировать robots.txt</button>
+</form>
+<?php
+const ROBOTS_SIZE = 32000;
+
+$host = $_GET['host'];
+$file_name = $host.'/robots.txt';
+
+$c = curl_init($file_name);
+curl_setopt($c, CURLOPT_RETURNTRANSFER, true);
+
+function getMax($content)
+{
+    $needle = 'User-agent';
+    $needleLow = "user-agent";
+    $res_arr = explode($needleLow, strtolower($content));
+    $max = 0;
+
+    foreach ($res_arr as $value) {
+        if ($value != "") {
+            $n = substr_count($value, "host");
+            if ($n > $max) {
+                $max = $n;
+            }
+        }
+    }
+    echo 'Max host is: '.$max.'<br>';
+    return $max;
+}
+
+$content = curl_exec($c);
+$info = curl_getinfo($c);
+$arrayInfo = array(
+    array('id' => 1, 'check' => 'Проверка наличия файла robots.txt', 'status' => 'unchecked'),
+    array('id' => 2, 'check' => 'Проверка указания директивы Host', 'status' => 'unchecked'),
+    array('id' => 3, 'check' => 'Проверка количества директив Host, прописанных в файле', 'status' => 'unchecked'),
+    array('id' => 4, 'check' => 'Проверка размера файла robots.txt', 'status' => 'unchecked'),
+    array('id' => 5, 'check' => 'Проверка указания директивы Sitemap', 'status' => 'unchecked'),
+    array('id' => 6, 'check' => 'Проверка кода ответа сервера для файла robots.txt', 'status' => 'unchecked')
+);
+if (!curl_errno($c)) {
+    if ($info['http_code'] != 200) {
+        foreach ($arrayInfo as $one) {
+            $one['status'] = 'Error';
+        }
+    } else {
+        $arrayInfo[0]['status'] = 'OK';
+        $arrayInfo[5]['status'] = 'OK';
+        if ($info['size_download'] > ROBOTS_SIZE) {
+            $arrayInfo[3]['status'] = 'Error';
+        } else {
+            $arrayInfo[3]['status'] = 'OK';
+        }
+
+    }
+    $res = stristr($content, 'Sitemap');
+
+    if ($res != '') {
+        $arrayInfo[4]['status'] = 'OK';
+    } elseif ($res == '') {
+        $arrayInfo[4]['status'] = 'Error';
+    }
+    // сделать проверку: если Host повторяется >1 раза но после User Agent то это не ошибка
+
+    $res = getMax($content);
+
+    if ($res == 1) {
+        $arrayInfo[1]['status'] = 'OK';
+        $arrayInfo[2]['status'] = 'OK';
+    } elseif ($res > 1) {
+        $arrayInfo[1]['status'] = 'OK';
+        $arrayInfo[2]['status'] = 'Error';
+    } else {
+        $arrayInfo[1]['status'] = 'Error';
+        $arrayInfo[2]['status'] = 'Error';
+    }
+    echo 'Took: ', $info['total_time'], ' seconds for request to ', $info['url'], "\n";
+    echo '<br>HTTP HEADER: ', $info['http_code'], "\n";
+    echo '<br>Data transfer size: ', $info['size_download'], "\n";
+
+
+    echo '<hr>';
+    echo '<pre>';
+    print_r($content);
+    echo '</pre>';
+
+}
+if ($info ['http_code'] != 200) {
+    echo "Error: ".$info ['http_code'];
+}
+curl_close($c);
+echo '<h5>Результат теста</h5>';
+echo '<table class="table table-striped table-bordered">';
+echo '<tr><th>id</th><th>Название проверки</th><th>Состояние</th></tr>';
+foreach ($arrayInfo as $array) {
+    echo '<tr>';
+    foreach ($array as $key => $value) {
+        echo '<td>'.$value.'</td>';
+    }
+    echo '</tr>';
+}
+echo '</table>';
+$_SESSION['arr'] = $arrayInfo;
+?>
+
+<form action="infoRobotsExcel.php" method="post">
+    <!--<label for="">Сохранить в Excel</label><br>-->
+    <button type="submit" class="btn btn-default" name="go">Сохранить в Excel</button>
+</form>
+</body>
+</html>
---
+++ infoRobots/infoRobotsExcel.php
@@ -0,0 +1,110 @@
+<?php
+session_start();
+// Подключаем библиотечный класс
+include("../Classes/PHPExcel.php");
+$data = $_SESSION['arr'];
+// Определяем массивы стилей:
+//$data[1]['status'] = 'Error';
+$style_arr = array(
+    'borders' => array(
+        'outline' => array(
+            'style' => PHPExcel_Style_Border::BORDER_THICK
+        ),
+        'allborders' => array(
+            'style' => PHPExcel_Style_Border::BORDER_THIN,
+            'color' => array('rgb' => '909090')
+        )
+    )
+);
+$style_center = array(
+    'alignment' => array(
+        'horizontal' => PHPExcel_Style_Alignment::HORIZONTAL_CENTER,
+        'vertical' => PHPExcel_Style_Alignment::VERTICAL_CENTER
+    )
+);
+$style_left = array(
+    'alignment' => array(
+        'horizontal' => PHPExcel_Style_Alignment::HORIZONTAL_LEFT,
+        'vertical' => PHPExcel_Style_Alignment::VERTICAL_CENTER
+    )
+);
+// Создать новый массив стилей, чтобы он был таким же как в примере тестового задания
+$style_ok = array(
+    'fill' => array(
+        'type' => PHPExcel_Style_Fill::FILL_SOLID,
+        'color' => array('rgb' => '93c47d')
+    )
+);
+$style_error = array(
+    'fill' => array(
+        'type' => PHPExcel_Style_Fill::FILL_SOLID,
+        'color' => array('rgb' => 'e06666')
+    )
+);
+$style_header = array(
+    'fill' => array(
+        'type' => PHPExcel_Style_Fill::FILL_SOLID,
+        'color' => array('rgb' => 'a2c4c9')
+    ),
+    'font' => array(
+        'name' => 'Arial',
+        'size' => 10,
+        'bold' => true,
+        'italic' => false
+    )
+);
+$style_between = array(
+    'fill' => array(
+        'type' => PHPExcel_Style_Fill::FILL_SOLID,
+        'color' => array('rgb' => 'efefef'),
+    )
+);
+
+$objExcel = new PHPExcel();
+$objExcel->setActiveSheetIndex(0);
+
+$active_sheet = $objExcel->getActiveSheet();
+$active_sheet->setTitle("Test Table");
+$active_sheet->getPageSetup()->setPaperSize(PHPExcel_Worksheet_PageSetup::PAPERSIZE_A4);
+$active_sheet->getPageSetup()->setOrientation(PHPExcel_Worksheet_PageSetup::ORIENTATION_PORTRAIT);
+
+$active_sheet->getColumnDimension('B')->setWidth(60);
+$active_sheet->getColumnDimension('B')->setWidth(30);
+
+$active_sheet->setCellValue('A1', '№');
+$active_sheet->setCellValue('B1', 'Название проверки');
+$active_sheet->setCellValue('C1', 'Статус');
+
+$objExcel->getActiveSheet()->getStyle('A:C')->applyFromArray($style_center);
+$objExcel->getActiveSheet()->getStyle('B')->applyFromArray($style_left);
+$objExcel->getActiveSheet()->getStyle('A1:C1')->applyFromArray($style_header);
+$active_sheet->getStyle('1')->getNumberFormat()->setFormatCode(PHPExcel_Style_NumberFormat::FORMAT_GENERAL);
+
+$num_row = 2;
+foreach ($data as $value) {
+    $active_sheet->mergeCells('A'.$num_row.':C'.$num_row);
+    $active_sheet->getStyle('A'.$num_row.':C'.$num_row)->applyFromArray($style_between);
+    $num_row++;
+    $active_sheet->getRowDimension($num_row)->setRowHeight(40);
+    $active_sheet->setCellValueByColumnAndRow(0, $num_row, $value['id']);
+    $active_sheet->setCellValueByColumnAndRow(1, $num_row, $value['check']);
+    $active_sheet->setCellValueByColumnAndRow(2, $num_row, $value['status']);
+    if ($value['status'] == 'OK') {
+        $objExcel->getActiveSheet()->getStyle('C'.$num_row)->applyFromArray($style_ok);
+    } else {
+        $objExcel->getActiveSheet()->getStyle('C'.$num_row)->applyFromArray($style_error);
+    }
+    // после каждой строки результата сделать пустую строку серого цвета
+    $num_row++;
+}
+
+$active_sheet->getStyle('A1:C'.($num_row - 1))->applyFromArray($style_arr);
+
+header('Content-Type: application/vnd.ms-excel');
+header('Content-Disposition: attachment; filename="our_table.xlsx"');
+
+$objWriter = PHPExcel_IOFactory::createWriter($objExcel, 'Excel2007');
+$objWriter->save('php://output');
+
+
+
